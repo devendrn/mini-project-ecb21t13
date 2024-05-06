7d90096754a1b87160710af6f4cbc921d9700952
devendrn <91605478+devendrn@users.noreply.github.com>
2024-05-06 05:41:45 -0400

    Use subparsers for commands

--- main.py
+++ main.py
@@ -1,34 +1,55 @@
 import argparse
-from scripts import compress, analysis, pack_score
+from scripts import compress, analysis, pack_score, plot
+
+
+def compressCmd(args):
+    compress.run()
+
+
+def analyzeCmd(args):
+    analysis.run()
+
+
+def scoreCmd(args):
+    pack_score.run()
+
+
+def plotCmd(args):
+    plot.run()
 
 
 def main():
     parser = argparse.ArgumentParser(description="Compression analysis")
-    parser.add_argument(
-        "--compress",
-        action="store_true",
+    subparsers = parser.add_subparsers(title="Commands", required=True)
+
+    compressParser = subparsers.add_parser(
+        "compress",
         help="Compression stage"
     )
-    parser.add_argument(
-        "--analyze",
-        action="store_true",
+    compressParser.set_defaults(func=compressCmd)
+
+    analyzeParser = subparsers.add_parser(
+        "analyze",
         help="Analysis stage"
     )
-    parser.add_argument(
-        "--score",
-        action="store_true",
-        help="Pack score"
+    analyzeParser.set_defaults(func=analyzeCmd)
+
+    scoreParser = subparsers.add_parser(
+        "score",
+        help="Pack score date"
     )
+    scoreParser.set_defaults(func=scoreCmd)
+
+    plotParser = subparsers.add_parser(
+        "plot",
+        help="Create plots from score data"
+    )
+    plotParser.set_defaults(func=plotCmd)
+
     args = parser.parse_args()
 
-    if args.compress:
-        compress.run()
-    elif args.analyze:
-        analysis.run()
-    elif args.score:
-        pack_score.run()
-    else:
-        print("No option specified. Use --help or -h to see usage.")
+    if hasattr(args, "func"):
+        args.func(args)
 
 
 if __name__ == "__main__":
